1b9669557110019ab48094d82bf0664ca9b6a642
camichaves <camichavesthg@gmail.com>
2020-06-30 18:00:23 -0300

    Agregando POOLEXECUTOR app1

--- oneVsAll.py
+++ oneVsAll.py
@@ -1,6 +1,19 @@
 from scipy import optimize
 from lrCostFunction import lrCostFunction
 import numpy as np
+import concurrent.futures
+
+
+def calculateThetas(c, X, y, lambda_, n):
+    initial_theta = np.zeros(n + 1)
+    options = {'maxiter': 50}
+    res = optimize.minimize(lrCostFunction,
+                            initial_theta,
+                            (X, (y == c), lambda_),
+                            jac=True,
+                            method='CG',
+                            options=options)
+    return res.x
 
 
 def oneVsAll(X, y, num_labels, lambda_):
@@ -9,18 +22,23 @@ def oneVsAll(X, y, num_labels, lambda_):
     all_theta = np.zeros((num_labels, n + 1))
 
     X = np.concatenate([np.ones((m, 1)), X], axis=1)
-
+    hijos = concurrent.futures.ProcessPoolExecutor()
+    hn = []
     for c in np.arange(num_labels):
-        initial_theta = np.zeros(n + 1)
-        options = {'maxiter': 50}
-        res = optimize.minimize(lrCostFunction,
-                                initial_theta,
-                                (X, (y == c), lambda_),
-                                jac=True,
-                                method='CG',
-                                options=options)
+        hn.append(hijos.submit(calculateThetas(), c, X, y, lambda_, n))
 
-        all_theta[c] = res.x
+    for f in concurrent.futures.as_completed(hn):
+        all_theta[f] = f.result()
+        #     initial_theta = np.zeros(n + 1)
+        #     options = {'maxiter': 50}
+        #     res = optimize.minimize(lrCostFunction,
+        #                             initial_theta,
+        #                             (X, (y == c), lambda_),
+        #                             jac=True,
+        #                             method='CG',
+        #                             options=options)
+
+        # all_theta[c] = res.x
 
     # ============================================================
     return all_theta

d1982fc4faf5e6c5fc1170f15a5c060ba3bf2307
camichaves <camichavesthg@gmail.com>
2020-06-30 10:54:53 -0300

    correcciones en predict

--- predictOneVsAll.py
+++ predictOneVsAll.py
@@ -3,12 +3,14 @@ import utils
 
 
 def predictOneVsAll(all_theta, X):
-    m = X.shape[0];
+    # Make sure the input has two dimensions
+    if X.ndim == 1:
+        X = X[None]  # promote to 2-dimensions
+    m = X.shape[0]
     num_labels = all_theta.shape[0]
     p = np.zeros(m)
     X = np.concatenate([np.ones((m, 1)), X], axis=1)
 
     p = np.argmax(utils.sigmoid(X.dot(all_theta.T)), axis=1)
 
-    # ============================================================
     return p
